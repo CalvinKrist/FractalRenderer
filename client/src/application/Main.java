e87df11e189506b13929b24e437e3fbca2af8454
Budder21 <darkkight21@gmail.com>
2017-04-16 00:26:08 -0400

    stuff

--- client/src/application/Main.java
+++ client/src/application/Main.java
@@ -1,20 +1,25 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			 Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		        primaryStage.setTitle("Hello World");
+		        primaryStage.setScene(new Scene(root));
+		        primaryStage.show();
+		/*	BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.show();
+			primaryStage.show();*/
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
