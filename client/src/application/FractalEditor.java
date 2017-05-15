90c6cbbf96480bdc270a8d2ba8698ee9e5f76a9a
David Smith <David Smith>
2017-05-15 13:39:56 -0400

    ieubwiuc

--- client/src/application/FractalEditor.java
+++ client/src/application/FractalEditor.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import javax.swing.SwingUtilities;
+
 import gui.Window;
 import javafx.embed.swing.SwingNode;
 import javafx.geometry.Insets;
@@ -16,8 +18,9 @@ import javafx.scene.layout.VBox;
 
 public class FractalEditor extends Scene {
 
-	BorderPane bp;
-	int width, height;
+	private BorderPane bp;
+	protected int width, height;
+	public Window gradient;
 
 	/**
 	 * This instantiates the Fractal Editor scene
@@ -33,34 +36,37 @@ public class FractalEditor extends Scene {
 		bp = (BorderPane) this.getRoot();
 		width = x;
 		height = y;
-		initialize();
+		//initialize();
 	}
 
-	private void initialize() throws FileNotFoundException {
+	public void initialize() throws FileNotFoundException {
 
 		ImageView fractalView = new ImageView();
 		fractalView.setImage(new Image(new FileInputStream("C:\\Users\\David\\Pictures\\butterfly.jpg")));
-		fractalView.setFitWidth(width-300);
+		fractalView.setFitWidth(width-200);
 		fractalView.setFitHeight(height-200);
 		
 		SwingNode fractalEditor = new SwingNode();
 		TreeView parameters = new TreeView();
 		TreeView layers = new TreeView();
 		VBox trees = new VBox();
-		Dimension d = new Dimension(width-300, 200);
+		Dimension d = new Dimension(width-350, 200);
 		Window gradient = new Window(d, 50);
-
-		fractalEditor.setContent(gradient);
-		fractalEditor.setOnMouseEntered(e -> gradient.repaint());
 		
+		fractalEditor.setOnMouseEntered(e -> gradient.repaint());
+		fractalEditor.setOnMouseClicked(e -> gradient.repaint());
 
 		trees.getChildren().addAll(parameters, layers);
+		trees.setPadding(new Insets(5));
 		
 		VBox center = new VBox();
 		center.getChildren().addAll(fractalView,fractalEditor);
+		center.setPadding(new Insets(20));
 
 		bp.setCenter(center);
 		bp.setRight(trees);
+		
+		SwingUtilities.invokeLater(() -> fractalEditor.setContent(gradient));
 	}
 	
 
