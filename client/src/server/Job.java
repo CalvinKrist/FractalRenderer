fe4e3700e596c9585dddea7ed0596d36bbcb5c45
= <calvin.krist@yahoo.com>
2017-06-04 11:27:16 -0400

    g

--- client/src/server/Job.java
+++ client/src/server/Job.java
@@ -16,6 +16,10 @@ import util.Parameters;
 public class Job implements Serializable {
 	
 	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8695628289122490725L;
+	/**
 	 * format of type_zoom
 	 */
 	private String jobId;
