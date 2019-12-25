71ddea8151348e2e23c6047abf3a7697ac671bd2
CalvinKrist <calvin.krist@yahoo.com>
2019-12-24 20:29:37 -0500

    Adds speed tests

--- fractals/README.md
+++ fractals/README.md
@@ -17,3 +17,28 @@ https://docs.python.org/3/c-api/exceptions.html
 
 Efficiently return buffer to python:
 https://stackoverflow.com/questions/28560045/best-way-to-copy-c-buffer-to-python
+
+## Speed Test Results
+
+The C calculations are how long it took to get a rendered result from the C modules with just a basic, single layer 'Simple Bands' type fractal.
+
+The Python render time is how long it took to then display those results in the rendered Python GUI that is used to develop palettes and find interesting parts of the fractal.
+
+### 12/24/2019
+```
+C calculations: 
+Average time 800x650 for 75 trials: 0.05229166666666667
+Correct: True
+-----------------------------
+Average time 1000x1000 for 50 trials: 0.10125
+Correct: True
+-----------------------------
+Average time 5000x5000 for 20 trials: 2.64375
+Correct: True
+-----------------------------
+Average time 10000x10000 for 5 trials: 16.425
+Correct: True
+
+Python Render Time
+Average time rendered in PyQt5: 0.9625
+```
---
+++ speed_test.py
@@ -0,0 +1,86 @@
+import fractal
+from time import process_time
+from view.FractalWindow import FractalRenderer
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication
+import sys
+
+
+def verify(width, height, value):
+    return True
+
+
+def test_c_by_size(frac, width, height, num=10):
+    # Get average time at 1000 by 1000
+    frac.width = width
+    frac.height = height
+
+    times = []
+    result = None
+
+    for i in range(num):
+        start = process_time()
+        result = frac.render()
+        end = process_time()
+        times.append(end - start)
+
+    average = sum(times) / len(times)
+    print("Average time " + str(width) + "x" + str(height) + " for " + str(num) + " trials: " + str(average))
+    print("Correct: " + str(verify(width, height, result)))
+
+
+class TimedFractalRenderer(FractalRenderer):
+    def __init__(self, frac):
+        super().__init__(frac)
+        self.result = self.fract.render()
+        self.elapsed = 0
+
+    def paintEvent(self, event):
+
+        image = self.result
+        cols = self.fract.width
+        rows = self.fract.height
+
+        start = process_time()
+
+        qp = QPainter()
+        qp.begin(self)
+        for y in range(rows):
+            for x in range(cols):
+                color = image[y * cols + x]
+                qp.fillRect(x, y, 1, 1, QColor(color[0], color[1], color[2]))
+        qp.end()
+
+        end = process_time()
+        self.elapsed = end - start
+
+def main():
+    frac = fractal.Fractal()
+
+    print("C calculations: ")
+    test_c_by_size(frac, 800, 650, 150)
+    print("-----------------------------")
+    test_c_by_size(frac, 1000, 1000, 75)
+    print("-----------------------------")
+    test_c_by_size(frac, 5000, 5000, 20)
+    print("-----------------------------")
+    test_c_by_size(frac, 10000, 10000, 5)
+
+    print()
+
+    print("Python Render Time")
+    app = QApplication(sys.argv)
+    frac = fractal.Fractal()
+    fractRenderer = TimedFractalRenderer(frac)
+
+    times = []
+
+    for i in range(30):
+        fractRenderer.paintEvent(None)
+        times.append(fractRenderer.elapsed)
+    average = sum(times) / len(times)
+    print("Average time rendered in PyQt5: " + str(average))
+
+
+if __name__ == '__main__':
+    main()
